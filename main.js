6dfd851308505fe271d82f7841716d934efd417a
Cumayevomr <cumayevomr8@gmail.com>
2025-08-15 01:48:28 +0400

    On scroll Function

---
+++ main.js
@@ -0,0 +1,39 @@
+
+// On scroll Function
+function navMenu() {
+    let navBar = document.querySelector('.navbar-sticky');
+    let scrollTopButton = document.querySelector('#scrollup');
+
+    window.onscroll = function () {
+        var scroll = document.documentElement.scrollTop;
+        if(scroll >= 120){
+            navBar.classList.add("navbar-sticky-moved-up");
+        } else {
+            navBar.classList.remove("navbar-sticky-moved-up");
+        }
+
+        //  Apply Transition
+        if(scroll >= 250){
+            navBar.classList.add("navbar-sticky-transitioned");
+            scrollTopButton.classList.add("scrollActive");
+        } else {
+            navBar.classList.remove("navbar-sticky-transitioned");
+            scrollTopButton.classList.remove("scrollActive");
+        }
+
+        // Sticky On
+        if(scroll >= 500){
+            navBar.classList.add("navbar-sticky-on");
+        } else {
+            navBar.classList.remove("navbar-sticky-on");
+        }
+    }
+}
+
+
+
+// counter design
+
+
+// APP-SCREEN
+
